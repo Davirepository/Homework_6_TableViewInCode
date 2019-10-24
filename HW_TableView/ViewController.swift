8ecc56a8ec6475762ad88d12019c086b97c8f035
David <bolotaev-david@mail.ru>
2019-10-24 15:56:09 +0300

    Initial Commit

---
+++ HW_TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_TableView
+//
+//  Created by Давид on 24/10/2019.
+//  Copyright © 2019 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
